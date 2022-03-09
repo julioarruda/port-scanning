ddca77fca79ae37f64ff7f7a3dfe18c60fa3b742
Julio Arruda <julio.arruda@outlook.com>
2022-03-09 20:28:47 -0300

    integer

--- scanning-one.py
+++ scanning-one.py
@@ -8,7 +8,7 @@ print ("Uso: python scanning.py 192.168.68.123 123")
 print ("IP Informado: ",sys.argv[1] )
 
 IP=sys.argv[1]
-PORT=sys.argv[2]
+PORT=int(sys.argv[2])
 
 meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if (meusocket.connect_ex((IP,PORT)) == 0):
