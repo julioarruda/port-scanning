0d1d09bc72763a5ef34602746ba46499e97c6c22
Julio Arruda <julio.arruda@outlook.com>
2022-03-09 20:12:51 -0300

    V1 - Scanning

---
+++ scanning.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import socket,sys
+
+print ("==================Escaneamento de Portas=================")
+
+print ("Uso: python scanning.py 192.168.68.123")
+
+for porta in range (1,65535):
+    meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if meusocket.connect((sys.argv[1],porta)) == 0:
+        print ("Porta: ",porta, " [ABERTA]")
+        meusocket.close()
