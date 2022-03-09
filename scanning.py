29c7c6d81d89fbf170a6e5cfd10f32db385f456b
Julio Arruda <julio.arruda@outlook.com>
2022-03-09 20:17:22 -0300

    test

--- scanning.py
+++ scanning.py
@@ -7,8 +7,10 @@ print ("Uso: python scanning.py 192.168.68.123")
 
 print ("IP Informado: ",sys.argv[1] )
 
+IP=sys.argv[1]
+
 for porta in range (1,65535):
     meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if meusocket.connect(sys.argv[1],porta) == 0:
+    if meusocket.connect(IP,porta) == 0:
         print ("Porta: ",porta, " [ABERTA]")
         meusocket.close()
